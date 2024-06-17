9a3c6d02b392aa386f07c341685aa43ec33792ce
jduncan017 <emailjoshduncan@gmail.com>
2024-06-17 14:50:04 -0600

    update ui

--- src/app/Components/hero.tsx
+++ src/app/Components/hero.tsx
@@ -9,7 +9,7 @@ interface HeroSectionProps {
 
 export default function HeroSection({ scrollToRef }: HeroSectionProps) {
   return (
-    <div className="HeroSection relative flex h-svh flex-col p-4 pt-[70px] xs:p-10">
+    <div className="HeroSection relative flex h-svh flex-col px-4 pt-[70px] xs:px-10">
       <div className="ImageContainer absolute inset-0 z-10 h-full overflow-hidden shadow-themeShadow sm:block">
         {/* mobile video */}
         <video
@@ -34,7 +34,7 @@ export default function HeroSection({ scrollToRef }: HeroSectionProps) {
           playsInline
         />
       </div>
-      <div className="TitleSection z-20 mx-auto mt-12 flex h-fit w-full flex-col gap-4 rounded-2xl bg-blurBlack p-6 py-12 text-white shadow-themeShadow backdrop-blur-lg backdrop-filter xs:mt-40 sm:max-w-[600px] sm:rounded-xl sm:px-12 sm:py-10">
+      <div className="TitleSection z-20 mx-auto mt-12 flex h-fit w-full flex-col gap-4 rounded-2xl bg-blurBlack p-6 py-12 text-white shadow-themeShadow backdrop-blur-lg backdrop-filter sm:max-w-[600px] sm:rounded-xl sm:px-12 sm:py-10">
         <div className="LogoSection flex h-fit flex-col items-center gap-2 pb-4">
           <Image
             className="Logo h-auto w-full sm:h-full sm:w-auto"

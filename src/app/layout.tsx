8e8ef13bee420b5b7c01f077c22a82e17f46a198
jduncan017 <emailjoshduncan@gmail.com>
2025-08-10 21:15:37 -0600

    update video sizing, add black bg to application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,7 +78,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-black">
       <head>
         {/* Preload critical fonts */}
         <link
